415d6bb2f0b45bf5db49b9d614e9a22ba842a24e
Oscar Negrete <oscar.negrete09@gmail.com>
2020-12-22 22:04:56 -0500

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -20,56 +26,61 @@ namespace API
     {
         private readonly IConfiguration _config;
 
-        public Startup (IConfiguration config)
+        public Startup(IConfiguration config)
         {
             _config = config;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration
+        {
+            get;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext> (options =>
-            {
-                options.UseSqlite (_config.GetConnectionString ("DefaultConnection"));
-            });
+            services.AddApplicationServices(_config);
 
-            services.AddControllers ();
-            services.AddCors ();
+            services.AddControllers();
+            services.AddCors();
+            services.AddIdentityServices(_config);
 
-            services.AddSwaggerGen (c =>
+            services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc ("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API", Version = "v1"
+                });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment ())
+            if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage ();
-                app.UseSwagger ();
-                app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "API v1"));
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection ();
+            app.UseHttpsRedirection();
 
-            app.UseRouting ();
+            app.UseRouting();
 
             // CORS Policy
-            app.UseCors (p =>
-                p.WithOrigins ("https://localhost:4200")
-                .AllowAnyHeader ()
-                .AllowAnyMethod ()
+            app.UseCors(p =>
+                p.WithOrigins("https://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
             );
 
-            app.UseAuthorization ();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-            app.UseEndpoints (endpoints =>
+            app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers ();
+                endpoints.MapControllers();
             });
         }
     }
